611a19a2264ee6152817138814ac3b76171b263f
Rawisara Saman <rawiisara@gmail.com>
2022-05-31 17:23:32 +0200

    header component with navbar added

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import "./Nav.css"
+const Nav = () => {
+    return (
+        <nav>
+          <ul>
+            <li>ABOUT</li>
+            <li>PORTFOLIO</li>
+            <li>CONTACT</li>
+          </ul>
+        </nav>
+                )
+}
+
+export default Nav
